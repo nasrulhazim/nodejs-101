b1a8d60597bf479e8de85699bb52294fdcb760ba
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-10-15 10:17:22 +0800

    test routing

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var http = require('http');
 
 var server = http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type":"text/plain"});
+
+	if(request.method == 'GET' && request.url == '/user') {
+		response.end('hello user');
+	}
+
 	response.end(request.method + ': ' + request.url);
 });
 
